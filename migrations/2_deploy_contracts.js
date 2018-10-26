1e28577741cb15d654519b7cc94a90f85a4deb85
dan sundman <44502165+sundexchange@users.noreply.github.com>
2018-10-26 21:43:48 +0200

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,9 @@
-var ETHERCToken = artifacts.require("./ETHERCToken.sol");
-var ETHERCFeeModifiers = artifacts.require("./ETHERCFeeModifiers.sol");
-var ETHERCExchange = artifacts.require("./ETHERCExchange.sol");
+var SUNDEXToken = artifacts.require("./SUNDEXToken.sol");
+var SUNDEXFeeModifiers = artifacts.require("./SUNDEXFeeModifiers.sol");
+var SUNDEXExchange = artifacts.require("./SUNDEXExchange.sol");
 
 module.exports = function(deployer, network, accounts) {
-  deployer.deploy(ETHERCToken);
-  deployer.deploy(ETHERCFeeModifiers);
-  deployer.deploy(ETHERCExchange);
+  deployer.deploy(SUNDEXToken);
+  deployer.deploy(SUNDEXFeeModifiers);
+  deployer.deploy(SUNDEXExchange);
 };
